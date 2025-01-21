55f6ad7416d63082f7ee303f3c139ec5befdeddb
ALEXOIME <alexisrobert.contact.pro@gmail.com>
2025-01-21 11:00:12 +0100

    refactor(config): update site URL and adjust plugins order
    Updated the site URL to 'alexisrobert.fr' and reorganized plugin imports for better readability. Adjusted the `noExternal` Vite configuration to include '@astrojs/react' and ensure compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import compressor from "astro-compressor";
 import vercel from '@astrojs/vercel/serverless';
-
+import tailwind from '@astrojs/tailwind';
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vigreux-joel.fr/",
+    site: "https://alexisrobert.fr/",
     compressHTML: true,
     experimental: {},
     output: 'hybrid',
@@ -19,7 +18,7 @@ export default defineConfig({
     }), sitemap(), robotsTxt(), compress(), compressor(),],
     vite: {
         ssr: {
-            noExternal: ["react-markdown", "@udixio/theme", "react-obfuscate", "react-google-recaptcha-v3"]
+            noExternal: ["@astrojs/react", "react-markdown", "react-obfuscate", "react-google-recaptcha-v3"],
         }
     },
     adapter: vercel()
